1647e4c4e8c3b5379b38e3491e99802c63f0b20e
Omar Linux <oayala775@gmail.com>
2024-11-25 15:27:43 -0600

    Correction of mistakes

--- publicacion/urls.py
+++ publicacion/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 app_name = 'publicacion'
 urlpatterns = [
-    path('', views.PublicacionView.as_view(), name='index'),
+    path('profesor/index', views.PublicacionView.as_view(), name='index'),
     path("profesor/<int:profesor_id>",views.ProfesorView.as_view(), name="profesor") #'/'
 ]
--- usuario/urls.py
+++ usuario/urls.py
@@ -4,6 +4,6 @@ from . import views
 #Establecer vistas y url correspondiente
 app_name = 'usuario'
 urlpatterns = [
-    path('registro', views.RegistriView.as_view(), name = 'registro'), #localhost/registro
-    path('inicio', views.InicioView.as_view(), name='inicio'), #localhost/inicio
+    path('registro', views.RegistroView.as_view(), name = 'registro'), #localhost/registro
+    path('', views.InicioView.as_view(), name='inicio'), #localhost/inicio
 ]
