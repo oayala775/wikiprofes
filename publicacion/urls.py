9276a6b30df7b6d6a8901af70a718f3b3c79f2ee
Uriel Alejandro Pérez Valdovinos <uriperez4142@gmail.com>
2024-11-26 22:04:39 -0600

    Crear reseña

--- publicacion/urls.py
+++ publicacion/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from . import views
 from usuario.views import logout_user
+from .views import PublicacionView, ProfesorView, MateriaView, PublicacionCreateView
 
 app_name = 'publicacion'
 urlpatterns = [
     path('', views.PublicacionView.as_view(), name='index'),
+    path('crear_reseña', PublicacionCreateView.as_view(), name='crear_reseña'),
     path("profesor/<int:profesor_id>",views.ProfesorView.as_view(), name="profesor"), #'/'
     path("materia/<int:materia_id>",views.MateriaView.as_view(), name="materia"),
     path("logout",logout_user, name="logout") #'/'
+   
 ]
