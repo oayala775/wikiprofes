a4b1d795da8235b7c8c3bcf23843318a6ba1cb44
Omar Linux <oayala775@gmail.com>
2024-11-28 11:49:21 -0600

    corr: fixed bug, when a teacher has no reviews, app brokes

--- publicacion/views.py
+++ publicacion/views.py
@@ -28,47 +28,56 @@ class ProfesorView(generic.ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["promedio_dominio"] = "%.1f" % (
-            Publicacion.objects.filter(
-                profesor_id=self.kwargs["profesor_id"]
-            ).aggregate(Avg("dominio", default=0))["dominio__avg"]
-        )
-
-        context["promedio_puntualidad"] = "%.1f" % (
-            Publicacion.objects.filter(
-                profesor_id=self.kwargs["profesor_id"]
-            ).aggregate(Avg("puntualidad", default=0))["puntualidad__avg"]
-        )
-
-        context["promedio_asistencia"] = "%.1f" % (
-            Publicacion.objects.filter(
-                profesor_id=self.kwargs["profesor_id"]
-            ).aggregate(Avg("asistencia", default=0))["asistencia__avg"]
-        )
-
-        context["promedio_dificultad"] = "%.1f" % (
-            Publicacion.objects.filter(
-                profesor_id=self.kwargs["profesor_id"]
-            ).aggregate(Avg("dificultad", default=0))["dificultad__avg"]
-        )
-
-        context["promedio_seguimiento"] = "%.1f" % (
-            Publicacion.objects.filter(
-                profesor_id=self.kwargs["profesor_id"]
-            ).aggregate(Avg("seguimiento", default=0))["seguimiento__avg"]
-        )
-
-        sumatoria = 0
         publicaciones = self.get_queryset()
-        for publicacion in publicaciones:
-            sumatoria += (
-                publicacion.dominio
-                + publicacion.puntualidad
-                + publicacion.asistencia
-                + publicacion.dificultad
-                + publicacion.seguimiento
-            ) / 5
-        context["promedio_general"] = "%.1f" % (sumatoria / len(publicaciones))
+
+        if publicaciones.exists():
+            context["promedio_dominio"] = "%.1f" % (
+                Publicacion.objects.filter(
+                    profesor_id=self.kwargs["profesor_id"]
+                ).aggregate(Avg("dominio", default=0))["dominio__avg"]
+            )
+
+            context["promedio_puntualidad"] = "%.1f" % (
+                Publicacion.objects.filter(
+                    profesor_id=self.kwargs["profesor_id"]
+                ).aggregate(Avg("puntualidad", default=0))["puntualidad__avg"]
+            )
+
+            context["promedio_asistencia"] = "%.1f" % (
+                Publicacion.objects.filter(
+                    profesor_id=self.kwargs["profesor_id"]
+                ).aggregate(Avg("asistencia", default=0))["asistencia__avg"]
+            )
+
+            context["promedio_dificultad"] = "%.1f" % (
+                Publicacion.objects.filter(
+                    profesor_id=self.kwargs["profesor_id"]
+                ).aggregate(Avg("dificultad", default=0))["dificultad__avg"]
+            )
+
+            context["promedio_seguimiento"] = "%.1f" % (
+                Publicacion.objects.filter(
+                    profesor_id=self.kwargs["profesor_id"]
+                ).aggregate(Avg("seguimiento", default=0))["seguimiento__avg"]
+            )
+
+            sumatoria = 0
+            for publicacion in publicaciones:
+                sumatoria += (
+                    publicacion.dominio
+                    + publicacion.puntualidad
+                    + publicacion.asistencia
+                    + publicacion.dificultad
+                    + publicacion.seguimiento
+                ) / 5
+            context["promedio_general"] = "%.1f" % (sumatoria / len(publicaciones))
+        else:
+            context["promedio_dominio"] = 0.0
+            context["promedio_puntualidad"] = 0.0
+            context["promedio_asistencia"] = 0.0
+            context["promedio_dificultad"] = 0.0
+            context["promedio_seguimiento"] = 0.0
+            context["promedio_general"] = 0.0
         return context
 
 class MateriaView(generic.ListView):
